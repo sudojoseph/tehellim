7ee60a0c524d4664bb9927bd4c4e4c6bb64961e0
joseph <josephdavidson@gmail.com>
2018-01-09 08:54:18 +0200

    functionality fixes

--- js.js
+++ js.js
@@ -1,43 +1,68 @@
-var url = "https://www.sefaria.org/api/texts/Psalms."
-var chapter = "";
-var engTextField = document.getElementById("english-text");
-var hebTextField = document.getElementById("hebrew-text");
-var eng = "";
-var heb = "";
 
-//AJAX call to Sefaria
+    var url = "https://www.sefaria.org/api/texts/Psalms."
+    var chapter = "";
+    var textField = document.getElementById("text");
+    var outputText = "";
+    var searchLanguage;
 
-function findTehellim() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            
-            var data = JSON.parse(this.responseText);
-            
-            //Create English Text
+    //AJAX call to Sefaria
 
-            for(i=0;i<data.text.length;i++) {
-               eng += "<div class='verse'>" + (i+1) + ". " + data.text[i] + "</div>"; 
+    function findTehellim() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+
+                var data = JSON.parse(this.responseText);
+
+                //Create English Text
+
+                if (searchLanguage === "eng"){
+                    for(i=0;i<data.text.length;i++) {
+                        outputText += "<div class='verse'>" + (i+1) + ". " + data.text[i] + "</div>";
+                    }
+                }
+
+                if (searchLanguage === "engHeb"){
+                    for(i=0;i<data.text.length;i++) {
+                        outputText += "<div class='verse'>" + (i+1) + ". " + data.he[i] + "</div>" + "<div class='verse'>" + (i+1) + ". " + data.text[i] + "</div>";
+                    }
+                }
+
+                if (searchLanguage === "heb"){
+                    for(i=0;i<data.text.length;i++) {
+                        outputText += "<div class='verse'>" + (i+1) + ". " + data.he[i] + "</div>";
+                    }
+                }
+
+                //Appending Generated Text To Page
+
+                textField.innerHTML = (outputText);
             }
+        };
+        xhttp.open("GET", url + chapter, true);
+        xhttp.send();
+    }
 
-            //Create Hebrew Text
+    function setOutput() {
+        outputText = "";
+        chapter = document.getElementById("chapterInput").value;
+        findTehellim();
+    }
 
-            for(i=0;i<data.he.length;i++) {
-               heb += "<div class='verse'>" + (i+1) + ". " + data.he[i] + "</div>"; 
-            }
-
-            //Appending Generated Text To Page
-
-            engTextField.innerHTML = (eng);
-            hebTextField.innerHTML = (heb);
-        }
+    document.getElementById('eng').onclick = function () {
+        searchLanguage = this.id;
+        console.log(searchLanguage);
+        setOutput();
     };
-    xhttp.open("GET", url + chapter, true);
-    xhttp.send();
-}
 
-document.getElementById("button").onclick = function () {
-    eng = heb = "";
-    chapter = document.getElementById("chapterInput").value;
-    findTehellim();
-};
+    document.getElementById('engHeb').onclick = function () {
+        searchLanguage = this.id;
+        console.log(searchLanguage);
+        setOutput();
+    };
+
+    document.getElementById('heb').onclick = function () {
+        searchLanguage = this.id;
+        console.log(searchLanguage);
+        setOutput();
+    };
